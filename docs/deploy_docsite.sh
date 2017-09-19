2d86bb3aaef2f4b4fa24efc96977af212d4b4325
vsen <vsen@expedia.com>
2017-09-19 15:29:22 +0530

    using an out sub-directory for generating

--- docs/deploy_docsite.sh
+++ docs/deploy_docsite.sh
@@ -9,28 +9,29 @@ REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
-# clone the existing gh-pages
-git clone $REPO haystack
-cd haystack/docs
-
 # build and generate documentation site
+# this would create a _book folder containing all the doc resources
+cd docs
 ./generate_docsite.sh
+cd ..
 
-# copy generated sources
-cp -R ./_book/* ..
+# Clone the existing gh-pages for this repo into out folder
+git clone $REPO out
+cd out
+git checkout $TARGET_BRANCH
 
-# If there are no changes to the compiled out then just bail.
-rm -rf ../deployment/
+# copy generated sources to root of out
+cp -R ../docs/_book/* .
+
+# if there are no changes to the generated out then just bail
 if git diff --quiet; then
     echo "No changes to the output on this push; exiting."
     exit 0
 fi
 
-# Commit the "changes", i.e. the new version.
-# The delta will show diffs between new and old versions.
-git clean -fx node_modules
-git add -A ..
+# Commit the new version of doc page
+git add -A .
 git commit -m "Deploy to GitHub Pages: ${SHA}"
 
-# Now that we're all set up, we can push.
-git push $SSH_REPO $SOURCE_BRANCH:$TARGET_BRANCH
+# now that we're all set up, push the changes to target branch
+git push $SSH_REPO $TARGET_BRANCH
