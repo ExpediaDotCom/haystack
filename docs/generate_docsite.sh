5d774ad6909869fbf1c87395f8860c05eb28d7be
vinay sen <vsen@expedia.com>
2017-09-19 20:12:20 +0530

    Travis build for autodeploying docs (#32)
    * adding deploy script and travis config
    
    * fixing script condition check
    
    * fixing script condition check
    
    * changing script permission
    
    * pushing to gh-pages-shadow branch
    
    * using relative path for copy
    
    * using an out sub-directory for generating
    
    * adding gh_token for pushing back to gh-pages branch
    
    * adding gh_ref for pushing to repo
    
    * updating git token
    
    * updating git token
    
    * using travis deploy for git pages deployment
    
    * removing redundant git token
    
    * removing token and use environment variable instead
    
    * removing deploy docsite script
    
    * using path relative to repo root
    
    * using path relative for script
    
    * removing path relative for out
    
    * moving to single travis script line
    
    * running deploy against master and gh-pages
    
    * going back to project root for deploy to work properly

--- docs/generate_docsite.sh
+++ docs/generate_docsite.sh
@@ -3,20 +3,9 @@
 # this script will use /docs folder as source of md files
 # will generate gitbook styled html files from md files
 # and get them ready for commit and push to gh-pages file
-# TODO will automate this process using a build job
 
 # install gitbook
 npm install -g gitbook-cli
 
 # install the plugins and build the static site
 gitbook install && gitbook build
-
-# copy the static site files into the root directory.
-cp -R _book/* ..
-
-# remove 'node_modules' and '_book' directory
-git clean -fx node_modules
-git clean -fx _book
-
-# add rest of the files
-git add ..
