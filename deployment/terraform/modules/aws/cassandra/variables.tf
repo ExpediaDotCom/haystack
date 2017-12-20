fa14cc5c9b1277ca570f9607344158445881530b
vsen <vsen@expedia.com>
2017-12-20 11:54:18 +0530

    improving cassandra tf template

--- deployment/terraform/modules/aws/cassandra/variables.tf
+++ deployment/terraform/modules/aws/cassandra/variables.tf
@@ -6,5 +6,9 @@ variable "cassandra_aws_vpc_id" {}
 variable "cassandra_aws_region" {}
 variable "cassandra_aws_subnet" {}
 variable "cassandra_security_group" {}
-variable "cassandra_ssh_key_pair_name" {}
 variable "cassandra_hosted_zone_id" {}
+variable "cassandra_ssh_key_pair_name" {}
+variable "cassandra_ssh_key_file_path" {}
+variable "cassandra_ssh_user" {
+  default = "ec2-user"
+}
