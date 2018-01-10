0916595d94e094d5536f7b87b9d40d41aee32060
Ayan Sen <ayansen89@gmail.com>
2018-01-10 18:00:11 +0530

    creating providers inside modules

--- deployment/terraform/cluster/aws/provider.tf
+++ deployment/terraform/cluster/aws/provider.tf
@@ -2,10 +2,6 @@ provider "aws" {
   region = "${var.aws_region}"
 }
 
-provider "kubernetes" {
-  config_context = "${module.haystack-k8s.cluster_name}"
-}
-
 data "aws_route53_zone" "haystack_dns_zone" {
   zone_id = "${var.aws_hosted_zone_id}"
 }
--- deployment/terraform/cluster/local/provider.tf
+++
@@ -1,3 +0,0 @@
-provider "kubernetes" {
-  config_context = "${var.k8s_minikube_cluster_name}"
-}
