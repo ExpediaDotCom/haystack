4d1845e53538a5d3c1fdd47792f3995ab530e2b0
Ayan Sen <ayansen89@gmail.com>
2018-01-11 16:00:11 +0530

    removing terraform context validation on terraform plan

--- deployment/terraform/cluster/aws/provider.tf
+++ deployment/terraform/cluster/aws/provider.tf
@@ -1,7 +1,10 @@
 provider "aws" {
   region = "${var.aws_region}"
 }
-provider "kubernetes" {}
+provider "kubernetes" {
+  config_context = "${module.haystack-k8s.cluster_name}"
+  load_config_file = false
+}
 
 data "aws_route53_zone" "haystack_dns_zone" {
   name = "${var.aws_domain_name}"
