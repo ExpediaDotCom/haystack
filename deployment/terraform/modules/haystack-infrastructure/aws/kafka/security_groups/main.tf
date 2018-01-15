2776a5c527d01af36d6cc891e45005a99d151af5
vsen <vsen@expedia.com>
2018-01-15 11:25:47 +0530

    adding zookeeper ingress ports in sg

--- deployment/terraform/modules/haystack-infrastructure/aws/kafka/security_groups/main.tf
+++ deployment/terraform/modules/haystack-infrastructure/aws/kafka/security_groups/main.tf
@@ -34,3 +34,31 @@ resource "aws_security_group_rule" "haytack-kafka-broker-egress" {
   protocol = "-1"
   cidr_blocks = ["0.0.0.0/0"]
 }
+
+resource "aws_security_group_rule" "haytack-kafka-broker-zookeeper-2888-ingress" {
+  type = "ingress"
+  security_group_id = "${aws_security_group.haystack-kafka.id}"
+  source_security_group_id = "${aws_security_group.haystack-kafka.id}"
+  from_port = 2888
+  to_port = 2888
+  protocol = "tcp"
+}
+
+resource "aws_security_group_rule" "haytack-kafka-broker-zookeeper-3888-ingress" {
+  type = "ingress"
+  security_group_id = "${aws_security_group.haystack-kafka.id}"
+  source_security_group_id = "${aws_security_group.haystack-kafka.id}"
+  from_port = 3888
+  to_port = 3888
+  protocol = "tcp"
+}
+
+resource "aws_security_group_rule" "haytack-kafka-broker-zookeeper-ingress" {
+  type = "ingress"
+  security_group_id = "${aws_security_group.haystack-kafka.id}"
+  from_port = 2181
+  to_port = 2181
+  protocol = "tcp"
+  cidr_blocks = [
+    "0.0.0.0/0"]
+}
