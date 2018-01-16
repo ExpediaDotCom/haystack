ab185b5e51346021f0965cd3e64c59d5df0fcf78
vsen <vsen@expedia.com>
2018-01-16 15:13:51 +0530

    creating separate zookeeper instances and multi-broker kafka cluster

--- deployment/terraform/modules/haystack-infrastructure/aws/kafka/main.tf
+++ deployment/terraform/modules/haystack-infrastructure/aws/kafka/main.tf
@@ -24,13 +24,45 @@ module "kafka-security-groups" {
   kafka_aws_vpc_id= "${var.kafka_aws_vpc_id}"
 }
 
+data "template_file" "zookeeper_user_data" {
+  template = "${file("${path.module}/data/zookeeper_user_data_sh.tpl")}"
+
+  vars {
+    haystack_graphite_host = "${var.kafka_graphite_host}"
+    haystack_graphite_port = "${var.kafka_graphite_port}"
+  }
+}
+
+// create zookeeper instance
+resource "aws_instance" "zookeeper_instance" {
+  count = "1"
+  ami = "${local.kafka_broker_ami}"
+  instance_type = "${var.kafka_broker_instance_type}"
+  subnet_id = "${var.kafka_aws_subnet}"
+  security_groups = [ "${module.kafka-security-groups.kafka_broker_security_group_ids}"]
+  key_name = "${var.kafka_ssh_key_pair_name}"
+
+  tags {
+    Name = "haystack-zookeeper-instance"
+    NodeType = "zookeeper"
+  }
+
+  root_block_device = {
+    volume_type = "gp2"
+    volume_size = "${var.kafka_broker_volume_size}"
+    delete_on_termination = false
+  }
+
+  user_data = "${data.template_file.zookeeper_user_data.rendered}"
+}
+
 data "template_file" "kafka_broker_user_data" {
   template = "${file("${path.module}/data/kafka_broker_user_data_sh.tpl")}"
 
   vars {
     haystack_graphite_host = "${var.kafka_graphite_host}"
     haystack_graphite_port = "${var.kafka_graphite_port}"
-    zookeeper_hosts = "localhost:2181"
+    zookeeper_hosts = "${aws_instance.zookeeper_instance.private_ip}:2181"
     num_partitions = "96"
     retention_hours = "24"
     retention_bytes = "${var.kafka_broker_volume_size * 805306368}"
@@ -48,7 +80,7 @@ resource "aws_instance" "haystack-kafka-broker" {
 
   tags {
     Name = "haystack-kafka-instance"
-    NodeType = "seed"
+    NodeType = "broker"
   }
 
   root_block_device = {
@@ -66,5 +98,5 @@ resource "aws_route53_record" "haystack-kafka-cname" {
   name    = "${local.kafka_cname}"
   type    = "A"
   ttl     = "300"
-  records = ["${aws_instance.haystack-kafka-broker.private_ip}"]
+  records = ["${aws_instance.haystack-kafka-broker.*.private_ip}"]
 }
