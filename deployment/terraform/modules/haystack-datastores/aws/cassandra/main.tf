6ccabcaca4a1641f3e9f524e862af7f3175c995c
vsen <vsen@expedia.com>
2018-01-24 18:47:55 +0530

    adding support for multi-seed cassandra cluster

--- deployment/terraform/modules/haystack-datastores/aws/cassandra/main.tf
+++ deployment/terraform/modules/haystack-datastores/aws/cassandra/main.tf
@@ -16,7 +16,6 @@ data "aws_ami" "haystack-cassandra-base-ami" {
 locals {
   cassandra_ami = "${var.node_image == "" ? data.aws_ami.haystack-cassandra-base-ami.image_id : var.node_image }"
   cassandra_config_yaml_path = "/etc/cassandra/default.conf/cassandra.yaml"
-  cassandra_non_seed_node_count = "${var.node_count - 1}"
   cassandra_cname = "${var.haystack_cluster_name}-cassandra"
   cassandra_ssh_user = "ec2-user"
 }
@@ -27,57 +26,66 @@ module "cassandra-security-groups" {
   haystack_cluster_name = "${var.haystack_cluster_name}"
 }
 
-data "template_file" "cassandra_seed_user_data" {
-  template = "${file("${path.module}/data/seed_node_user_data_sh.tpl")}"
+data "template_file" "cassandra_user_data" {
+  template = "${file("${path.module}/data/node_user_data_sh.tpl")}"
 
   vars {
+    seed_node_count = "${var.seed_node_count}"
+    clusterRole = "${var.haystack_cluster_name}-cassandra-seed"
     haystack_graphite_host = "${var.graphite_host}"
     haystack_graphite_port = "${var.graphite_port}"
   }
 }
 
-// create seed node
-resource "aws_instance" "haystack-cassandra-seed-node" {
+resource "aws_iam_role" "haystack-cassandra-role" {
+  name = "${var.haystack_cluster_name}-cassandra-nodes-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": { "Service": "ec2.amazonaws.com"},
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+  EOF
+}
+
+resource "aws_iam_role_policy" "cassandra-policy" {
+  name = "cassandra-policy"
+  role = "${aws_iam_role.haystack-cassandra-role.name}"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "cassandraRoute53ListZones",
+      "Effect": "Allow",
+      "Action": [
+        "ec2:DescribeInstances"
+      ],
+      "Resource": [
+        "*"
+      ]
+    }
+  ]
+}
+  EOF
+}
+
+resource "aws_iam_instance_profile" "haystack-cassandra-nodes-profile" {
+  name = "${var.haystack_cluster_name}-cassandra-nodes"
+  role = "${aws_iam_role.haystack-cassandra-role.name}"
+}
+
+resource "aws_instance" "haystack-cassandra-seed-nodes" {
+  count = "${var.seed_node_count}"
   ami = "${local.cassandra_ami}"
   instance_type = "${var.node_instance_type}"
   subnet_id = "${var.aws_subnet}"
-  vpc_security_group_ids = [ "${module.cassandra-security-groups.nodes_security_group_ids}"]
-  key_name = "${var.aws_ssh_key_pair_name}"
-  associate_public_ip_address = false
-  tags = {
-    Product = "Haystack"
-    Component = "Cassandra"
-    ClusterName = "${var.haystack_cluster_name}"
-    Role = "${var.haystack_cluster_name}-cassandra"
-    Name = "${var.haystack_cluster_name}-cassandra"
-    NodeType = "seed"
-  }
-
-  root_block_device = {
-    volume_type = "gp2"
-    volume_size = "${var.node_volume_size}"
-    delete_on_termination = false
-  }
-
-  user_data = "${data.template_file.cassandra_seed_user_data.rendered}"
-}
-
-data "template_file" "cassandra_non_seed_user_data" {
-  template = "${file("${path.module}/data/non_seed_node_user_data_sh.tpl")}"
-
-  vars {
-    seed_ip = "${aws_instance.haystack-cassandra-seed-node.private_ip}"
-    haystack_graphite_host = "${var.graphite_host}"
-    haystack_graphite_port = "${var.graphite_port}"
-  }
-}
-
-// use seed node ip to create & initialize non-seed nodes
-resource "aws_instance" "haystack-cassandra-non-seed-nodes" {
-  count = "${local.cassandra_non_seed_node_count}"
-  ami = "${local.cassandra_ami}"
-  instance_type = "${var.node_instance_type}"
-  subnet_id = "${var.aws_subnet}"
+  iam_instance_profile = "${aws_iam_instance_profile.haystack-cassandra-nodes-profile.name}"
   vpc_security_group_ids = [ "${module.cassandra-security-groups.nodes_security_group_ids}"]
   associate_public_ip_address = false
   key_name = "${var.aws_ssh_key_pair_name}"
@@ -88,8 +96,7 @@ resource "aws_instance" "haystack-cassandra-non-seed-nodes" {
     ClusterName = "${var.haystack_cluster_name}"
     Role = "${var.haystack_cluster_name}-cassandra"
     Name = "${var.haystack_cluster_name}-cassandra-${count.index}"
-    Seed = "${aws_instance.haystack-cassandra-seed-node.private_ip}"
-    NodeType = "non-seed"
+    ClusterRole = "${var.haystack_cluster_name}-cassandra-seed"
   }
 
   root_block_device = {
@@ -98,7 +105,35 @@ resource "aws_instance" "haystack-cassandra-non-seed-nodes" {
     delete_on_termination = false
   }
 
-  user_data = "${data.template_file.cassandra_non_seed_user_data.rendered}"
+  user_data = "${data.template_file.cassandra_user_data.rendered}"
+}
+
+resource "aws_instance" "haystack-cassandra-non-seed-nodes" {
+  count = "${var.non_seed_node_count}"
+  ami = "${local.cassandra_ami}"
+  instance_type = "${var.node_instance_type}"
+  subnet_id = "${var.aws_subnet}"
+  iam_instance_profile = "${aws_iam_instance_profile.haystack-cassandra-nodes-profile.name}"
+  vpc_security_group_ids = [ "${module.cassandra-security-groups.nodes_security_group_ids}"]
+  associate_public_ip_address = false
+  key_name = "${var.aws_ssh_key_pair_name}"
+
+  tags {
+    Product = "Haystack"
+    Component = "Cassandra"
+    ClusterName = "${var.haystack_cluster_name}"
+    Role = "${var.haystack_cluster_name}-cassandra"
+    Name = "${var.haystack_cluster_name}-cassandra-${var.seed_node_count + count.index}"
+    ClusterRole = "${var.haystack_cluster_name}-cassandra-non-seed"
+  }
+
+  root_block_device = {
+    volume_type = "gp2"
+    volume_size = "${var.node_volume_size}"
+    delete_on_termination = false
+  }
+
+  user_data = "${data.template_file.cassandra_user_data.rendered}"
 }
 
 // create cname for newly created cassandra cluster
@@ -107,5 +142,5 @@ resource "aws_route53_record" "haystack-cassandra-cname" {
   name    = "${local.cassandra_cname}"
   type    = "A"
   ttl     = "300"
-  records = ["${aws_instance.haystack-cassandra-seed-node.private_ip}","${aws_instance.haystack-cassandra-non-seed-nodes.*.private_ip}"]
+  records = ["${concat(aws_instance.haystack-cassandra-seed-nodes.*.private_ip, aws_instance.haystack-cassandra-non-seed-nodes.*.private_ip)}"]
 }
