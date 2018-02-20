2720f8a01750aa6bc3d11ac281548e0474378091
jeffbaker <jeffbaker@expedia.com>
2018-02-20 15:15:46 -0800

    Add configurations, variables, etc. for Firehose Writer.

--- deployment/terraform/modules/haystack-apps/kubernetes/variables.tf
+++ deployment/terraform/modules/haystack-apps/kubernetes/variables.tf
@@ -30,11 +30,16 @@ variable "trends_version" {}
 variable "pipes_json_transformer_enabled" {}
 variable "pipes_kafka_producer_enabled" {}
 variable "pipes_http_poster_enabled" {}
+variable "pipes_firehose_writer_enabled" {}
 variable "pipes_json_transformer_instances" {}
 variable "pipes_kafka_producer_instances" {}
 variable "pipes_http_poster_instances" {}
 variable "pipes_http_poster_httppost_url" {}
 variable "pipes_http_poster_httppost_pollpercent" {}
+variable "pipes_firehose_writer_instances" {}
+variable "pipes_firehose_writer_firehose_url" {}
+variable "pipes_firehose_writer_firehose_streamname" {}
+variable "pipes_firehose_writer_firehose_signingregion" {}
 variable "pipes_version" {}
 
 # collectors config
