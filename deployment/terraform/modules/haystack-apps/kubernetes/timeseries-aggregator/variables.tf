35669c5af632254b4ef000d6d6f0dcd80bad838f
Ayan Sen <ayansen89@gmail.com>
2018-02-23 15:38:12 +0530

    added support for external metrictank broker

--- deployment/terraform/modules/haystack-apps/kubernetes/timeseries-aggregator/variables.tf
+++ deployment/terraform/modules/haystack-apps/kubernetes/timeseries-aggregator/variables.tf
@@ -5,6 +5,8 @@ variable "kafka_endpoint" {}
 variable "graphite_hostname" {}
 variable "graphite_port" {}
 variable "enabled"{}
+variable "enable_external_kafka_producer" {}
+variable "external_kafka_producer_endpoint" {}
 
 variable "node_selecter_label" {
   type = "map"
