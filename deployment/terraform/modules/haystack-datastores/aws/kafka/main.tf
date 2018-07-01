7a6ad98cd091a3872b09688ff878dd921f42b5c4
Mohamad Salah Abdulmuhsen Olabi <molabi@expedia.com>
2018-07-01 18:02:55 +0300

    modify subnet variable

--- deployment/terraform/modules/haystack-datastores/aws/kafka/main.tf
+++ deployment/terraform/modules/haystack-datastores/aws/kafka/main.tf
@@ -127,7 +127,7 @@ resource "aws_instance" "haystack-kafka-broker" {
   ami = "${local.kafka_broker_ami}"
   instance_type = "${var.broker_instance_type}"
   #subnet_id = "${var.aws_subnet}"
-  subnet_id = "${element(var.subnet_ids, count.index)}"
+  subnet_id = "${element(var.aws_subnets, count.index)}"
   vpc_security_group_ids = [ "${module.kafka-security-groups.kafka_broker_security_group_ids}"]
   key_name = "${var.aws_ssh_key_pair_name}"
   associate_public_ip_address = false
