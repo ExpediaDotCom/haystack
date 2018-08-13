105edc49dde5f5bb238ec270d110aad6a6cee698
Willie Wheeler <willie.wheeler@gmail.com>
2018-08-13 18:52:25 +1000

    Reinstated aquila modules

---
+++ deployment/terraform/modules/haystack-apps/kubernetes/alerting/aquila-detect/variables.tf
@@ -0,0 +1,30 @@
+# Kubernetes
+variable "namespace" {}
+variable "enabled" {}
+variable "replicas" {}
+variable "cpu_limit" {}
+variable "cpu_request" {}
+variable "memory_limit" {}
+variable "memory_request" {}
+variable "node_selector_label" {}
+variable "kubectl_executable_name" {}
+variable "kubectl_context_name" {}
+
+# Docker
+variable "image" {}
+variable "image_pull_policy" {}
+
+# Environment
+variable "jvm_memory_limit" {}
+variable "graphite_hostname" {}
+variable "graphite_port" {}
+variable "graphite_enabled" {}
+variable "env_vars" {}
+
+# Service
+variable "service_port" {
+  default = 80
+}
+variable "container_port" {
+  default = 8080
+}
