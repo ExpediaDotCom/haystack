0041c38691f7c2d1b4634118625c0517a9006311
Willie Wheeler <willie.wheeler@gmail.com>
2018-10-24 15:28:22 +1000

    AA version bumps (#569)

--- deployment/terraform/cluster/aws/apps/variables.tf
+++ deployment/terraform/cluster/aws/apps/variables.tf
@@ -210,7 +210,7 @@ variable "alerting" {
   type = "map"
   default = {
     enabled = false
-    version = "3fc6132075449a7a054f6db71e248abc9cbd0305"
+    version = "b5999890e29939f068ea0e2106cfaa4d4f605e6c"
   }
 }
 
@@ -234,7 +234,7 @@ variable "ad-mapper" {
   default = {
     enabled = false
     instances = 1
-    image = "expediadotcom/adaptive-alerting-ad-mapper:66a7d28cc90ad1c3f54db078e849c962e4771289"
+    image = "expediadotcom/adaptive-alerting-ad-mapper:b5999890e29939f068ea0e2106cfaa4d4f605e6c"
     image_pull_policy = "IfNotPresent"
     cpu_request = "500m"
     cpu_limit = "2000m"
@@ -251,7 +251,7 @@ variable "ad-manager" {
   default = {
     enabled = false
     instances = 1
-    image = "expediadotcom/adaptive-alerting-ad-manager:66a7d28cc90ad1c3f54db078e849c962e4771289"
+    image = "expediadotcom/adaptive-alerting-ad-manager:b5999890e29939f068ea0e2106cfaa4d4f605e6c"
     image_pull_policy = "IfNotPresent"
     cpu_request = "500m"
     cpu_limit = "2000m"
@@ -310,7 +310,7 @@ variable "aquila-detector" {
   default = {
     enabled = false
     instances = 1
-    image = "expediadotcom/aquila-detector:66a7d28cc90ad1c3f54db078e849c962e4771289"
+    image = "expediadotcom/aquila-detector:b5999890e29939f068ea0e2106cfaa4d4f605e6c"
     image_pull_policy = "IfNotPresent"
     cpu_request = "500m"
     cpu_limit = "2000m"
@@ -330,7 +330,7 @@ variable "aquila-trainer" {
   default = {
     enabled = false
     instances = 1
-    image = "expediadotcom/aquila-trainer:66a7d28cc90ad1c3f54db078e849c962e4771289"
+    image = "expediadotcom/aquila-trainer:b5999890e29939f068ea0e2106cfaa4d4f605e6c"
     image_pull_policy = "IfNotPresent"
     cpu_request = "500m"
     cpu_limit = "2000m"
