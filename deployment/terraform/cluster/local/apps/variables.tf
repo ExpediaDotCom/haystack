1fc3cfd288a4b38f135ff598e24a3884d9bbe64c
Ashish Aggarwal <asaggarwal@expedia.com>
2019-04-01 23:58:02 +0530

    allow extra env variables for storage backends (#696)

--- deployment/terraform/cluster/local/apps/variables.tf
+++ deployment/terraform/cluster/local/apps/variables.tf
@@ -40,9 +40,7 @@ variable "traces" {
     backend_memory_request = "250"
     backend_memory_limit = "250"
     backend_jvm_memory_limit = "200"
-    backend_cassandra_retries_max = 10
-    backend_cassandra_backoff_initial_ms = 100
-    backend_cassandra_backoff_factor = 1
+    backend_environment_overrides = ""
   }
 }
 
