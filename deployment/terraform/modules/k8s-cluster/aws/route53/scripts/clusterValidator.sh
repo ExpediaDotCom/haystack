7a1eab45bc334cd68afb9457929a378655e41902
Abhishek Srivastava <absrivastava@expedia.com>
2019-04-30 12:00:37 +0530

    adding script

---
+++ deployment/terraform/modules/k8s-cluster/aws/route53/scripts/clusterValidator.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+kubectl="$1"
+shouldExit="false"
+while [[ "$shouldExit" == "false" ]]
+   do
+    nodes=$(${kubectl} get nodes -l kubernetes.io/role=master -o json  | jq -r .items)
+    shouldExit="true"
+
+    for row in $(echo "${nodes}" | jq -r '.[] | @base64'); do
+    _jq() {
+     echo ${row} | base64 --decode | jq -r ${1}
+    }
+
+   conditions=$(echo $(_jq '.status.conditions'))
+
+   readyNodes=$(echo ${conditions} | jq -e '.[] | select(.type=="Ready" and .status=="True")')
+   exit_status=$(echo $?)
+
+   echo $readyNodes
+   if [[ $exit_status -gt 0 ]]; then
+    echo "false"
+    shouldExit="false"
+    break
+   fi
+
+   done
+   sleep 5
+done
+
+
