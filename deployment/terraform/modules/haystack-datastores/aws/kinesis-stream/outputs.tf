2a39e812dbffcc1e77a01d7a43f66a7ef642a082
Abhishek Srivastava <absrivastava@expedia.com>
2019-05-13 14:32:22 +0530

    fixing outputs

--- deployment/terraform/modules/haystack-datastores/aws/kinesis-stream/outputs.tf
+++ deployment/terraform/modules/haystack-datastores/aws/kinesis-stream/outputs.tf
@@ -1,13 +1,13 @@
 output "kinesis-stream_arn" {
-  value = "${aws_kinesis_stream.kinesis-spans-stream.arn}"
+  value = "${join("", aws_kinesis_stream.kinesis-spans-stream.*.arn)}"
 }
 
 output "kinesis-stream_name" {
-  value = "${aws_kinesis_stream.kinesis-spans-stream.name}"
+  value = "${join("", aws_kinesis_stream.kinesis-spans-stream.*.name)}"
 }
 
 output "kinesis-stream_shardcount" {
-  value = "${aws_kinesis_stream.kinesis-spans-stream.shard_count}"
+  value = "${join("", aws_kinesis_stream.kinesis-spans-stream.*.shard_count)}"
 }
 
 output "kinesis-stream_region" {
