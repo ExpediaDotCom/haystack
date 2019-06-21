6d42379c0b84599d3d4e29b8c5f2dff6e57d2b7d
Raghvendra Singh <raghvendra.iiita.work@gmail.com>
2019-06-21 11:59:49 +0530

    adding the depends for when the resources are not computed

--- deployment/terraform/modules/haystack-datastores/aws/elasticsearch/outputs.tf
+++ deployment/terraform/modules/haystack-datastores/aws/elasticsearch/outputs.tf
@@ -1,5 +1,6 @@
 output "elasticsearch_hostname" {
   value = "${aws_elasticsearch_domain.haystack_index_store.endpoint}"
+  depends_on = ["aws_elasticsearch_domain.haystack_index_store"]
 }
 
 output "elasticsearch_service_port" {
