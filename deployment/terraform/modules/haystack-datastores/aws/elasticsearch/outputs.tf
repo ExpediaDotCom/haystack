30f47eacfcb389e25e8877c56182dfb2887e89f0
Abhishek Srivastava <absrivastava@expedia.com>
2019-07-30 15:12:23 +0530

    Adding configuration for Creating Extra ES to be used for Scaling (#760)
    * Adding configuration for Creating Extra ES to be used for Scaling
    
    * Adding configuration for Creating Extra ES to be used for Scaling
    
    * fixing outputs for ES
    
    * fixing output

--- deployment/terraform/modules/haystack-datastores/aws/elasticsearch/outputs.tf
+++ deployment/terraform/modules/haystack-datastores/aws/elasticsearch/outputs.tf
@@ -3,6 +3,16 @@ output "elasticsearch_hostname" {
   depends_on = ["aws_elasticsearch_domain.haystack_index_store"]
 }
 
+output "elasticsearch_hostnames" {
+  value = "${join("," , list("${join("", aws_elasticsearch_domain.haystack_index_store.*.endpoint)}","${join("", aws_elasticsearch_domain.a-haystack_index_store.*.endpoint)}"))}"
+  depends_on = ["aws_elasticsearch_domain.haystack_index_store", "aws_elasticsearch_domain.a-haystack_index_store"]
+}
+
+output "elasticsearch_a-hostname" {
+  value = "${join("", aws_elasticsearch_domain.a-haystack_index_store.*.endpoint)}"
+  depends_on = ["aws_elasticsearch_domain.a-haystack_index_store"]
+}
+
 output "elasticsearch_service_port" {
   value = "80"
 }
