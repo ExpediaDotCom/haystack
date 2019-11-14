692b077e59817e88db390a949495cc1e7880f31c
keshavpeswani <keshavpeswani@gmail.com>
2019-11-14 13:09:00 +0530

    Fixing the cpu limit

--- deployment/terraform/cluster/aws/infrastructure/variables.tf
+++ deployment/terraform/cluster/aws/infrastructure/variables.tf
@@ -11,7 +11,7 @@ variable "monitoring_addons" {
     grafana_root_url = ""
     influxdb_storage_volume = "50Gi"
     influxdb_memory_limit = "2Gi"
-    influxdb_cpu_limit = "500"
+    influxdb_cpu_limit = "500m"
   }
 }
 
